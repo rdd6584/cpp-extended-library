080fe8f3db6f3b5b3121c067b67dd2b842544f27
rdd6584 <rdd573@naver.com>
2022-08-03 03:41:24 +0900

    Added, combinatorics class. - When solving algorithm problem, Almost these problem needs modular arithmetic. - So, I decided the class require modular. Signed-off-by: rdd6584 <rdd573@naver.com>

---
+++ src/cpp_extended_stl/combinatorics.hpp
@@ -0,0 +1,116 @@
+/* MIT License
+Copyright (c) 2022 rdd6584 */
+
+#ifndef CPP_EXTENDED_STL_COMBINATORICS_
+#define CPP_EXTENDED_STL_COMBINATORICS_
+
+#include <vector>
+
+namespace cpp_extended_stl {
+
+class Combinatorics {
+    typedef long long ll;
+
+   private:
+    std::vector<ll> factorial_, factorial_inverse_;
+    ll modular_;
+
+   public:
+    Combinatorics(int modular) { modular_ = modular; }
+
+    ll FastPower(ll base, ll exponent) const {
+        ll result = 1;
+        while (exponent) {
+            if (exponent & 1)
+                result = result * base % modular_;
+            base = base * base % modular_;
+            exponent >>= 1;
+        }
+        return result;
+    }
+    ll GetInverse(const ll number) const {
+        return FastPower(number, modular_ - 2);
+    }
+
+    // Please, ensure you did InitializeFactorial(greater than, or equal to n);
+    ll Factorial(int n) const { return factorial_[n]; }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to n);
+    ll FactorialInverse(int n) const { return factorial_inverse_[n]; }
+
+    void InitializeFactorial(int n) {
+        if (factorial_.size() >= n + 1) {
+            return;
+        }
+
+        int previous_n = factorial_.size();
+        factorial_.resize(n + 1);
+
+        factorial_[0] = 1;
+        for (int i = std::max(1, previous_n); i <= n; i++)
+            factorial_[i] = factorial_[i - 1] * i % modular_;
+    }
+    void InitializeFactorialWithInverse(int n) {
+        if (factorial_inverse_.size() >= n + 1) {
+            return;
+        }
+
+        InitializeFactorial(n);
+
+        int previous_n = factorial_inverse_.size();
+        factorial_inverse_.resize(n + 1);
+
+        factorial_inverse_[0] = 1;
+        factorial_inverse_[n] = GetInverse(factorial_[n]);
+
+        for (int i = n; i > previous_n; i--)
+            factorial_inverse_[i - 1] = (factorial_inverse_[i] * i) % modular_;
+    }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to n);
+    ll Permutation(int n, int r) const {
+        return (r > n || r < 0) ? 0 : factorial_[n] * factorial_inverse_[n - r] % modular_;
+    }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to n);
+    ll Combination(int n, int r) const {
+        return (r > n || r < 0) ? 0 : factorial_[n] * factorial_inverse_[r] % modular_ * factorial_inverse_[n - r] % modular_;
+    }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to n + r - 1);
+    ll CombinationWithRepetition(int n, int r) const {
+        return Combination(n + r - 1, r);
+    }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to 2 * n);
+    ll CatalanNumber(int n) {
+        return Combination(2 * n, n) * GetInverse(n + 1) % modular_;
+    }
+
+    // Please, ensure you did InitializeFactorialWithInverse(greater than, or equal to n);
+    ll DerangementNumber(int n) {
+        ll result = 0;
+
+        for (int k = 0, sign = 1; k <= n; k++, sign = -sign)
+            result = (result + (factorial_[n] * sign * factorial_inverse_[k]) % modular_ + modular_) % modular_;
+        return result;
+    }
+
+    // static
+    static ll FastPower(ll base, ll exponent, const ll modular) {
+        ll result = 1;
+        while (exponent) {
+            if (exponent & 1)
+                result = result * base % modular;
+            base = base * base % modular;
+            exponent >>= 1;
+        }
+        return result;
+    }
+    static ll GetInverse(const ll number, const ll modular) {
+        return FastPower(number, modular - 2, modular);
+    }
+};
+}  // namespace cpp_extended_stl
+
+#endif  // CPP_EXTENDED_STL_COMBINATORICS_
--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,19 +1,58 @@
+#include <assert.h>
 #include <cstdio>
+#include "combinatorics.hpp"
 #include "fraction.hpp"
 typedef cpp_extended_stl::Fraction Fraction;
+typedef cpp_extended_stl::Combinatorics Combinatorics;
 
-int main() {
+void FractionTest() {
     Fraction af, bf;
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int a = 1, b = 5, c = -3, d = 2;
 
     af = {a, b};
     bf = {c, d};
 
-    printf("%s\n", (af + bf).GetString().c_str());
-    printf("%s\n", (af - bf).GetString().c_str());
-    printf("%s\n", (af * bf).GetString().c_str());
-    printf("%s\n", (af / bf).GetString().c_str());
-    printf("%d %d\n", af > bf, af < bf);
-    printf("%d\n", af == bf);
+    assert((af + bf).GetString() == "-13/10");
+    assert((af - bf).GetString() == "17/10");
+    assert((af * bf).GetString() == "-3/10");
+    assert((af / bf).GetString() == "-2/15");
+    assert((af > bf) == true);
+    assert((af < bf) == false);
+    assert((af == bf) == false);
+}
+
+void CombinatoricsTest() {
+    Combinatorics combinatorics(1e9 + 7);
+    combinatorics.InitializeFactorialWithInverse(1001);
+
+    assert(combinatorics.Factorial(0) == 1);
+    assert(combinatorics.GetInverse(1) == 1);
+
+    assert(combinatorics.FastPower(10, 3) == 1000);
+
+    assert(combinatorics.Factorial(10) == 3628800);
+    assert(combinatorics.Combination(20, 10) == 184756);
+    assert(combinatorics.CombinationWithRepetition(20, 10) == 20030010);
+
+    assert(combinatorics.CatalanNumber(0) == 1);
+    assert(combinatorics.CatalanNumber(1) == 1);
+    assert(combinatorics.CatalanNumber(2) == 2);
+    assert(combinatorics.CatalanNumber(3) == 5);
+    assert(combinatorics.CatalanNumber(4) == 14);
+
+    assert(combinatorics.DerangementNumber(1) == 0);
+    assert(combinatorics.DerangementNumber(2) == 1);
+    assert(combinatorics.DerangementNumber(5) == 44);
+    assert(combinatorics.DerangementNumber(6) == 265);
+    assert(combinatorics.DerangementNumber(7) == 1854);
+
+    assert(combinatorics.Permutation(5, 2) == 20);
+}
+
+int main() {
+    FractionTest();
+    printf("Passed FractionTest !\n");
+
+    CombinatoricsTest();
+    printf("Passed CombinatoricsTest !");
 }
